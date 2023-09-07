9e9bc9db6d865dd0c821c77a101e8b1b4f8ab4ea
Elison-schumacher <elisonschumacher30@gmail.com>
2023-09-07 10:17:38 -0300

    ANALIZAR

---
+++ scripts.js
@@ -0,0 +1,26 @@
+const convertButton = document.querySelector(".convertButton")
+
+function convertValues() {
+    const input = document.querySelector(".input").value //valor que colocar no conversor 
+    const coinVelue = document.querySelector(".coin-converter") //valor em REAL
+    const coinConverter = document.querySelector(".coin-moedas") //valor em DÓLAR 
+
+
+    const dolarToday = 5.20
+
+    const covertedValue = input / dolarToday
+
+    coinVelue.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(input)
+
+    coinConverter.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+    }).format(convertValues)
+
+    console.log(covertedValue)
+}
+
+convertButton.addEventListener("click", convertValues)
