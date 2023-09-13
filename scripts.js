c4ab8a2a931a4c888af0f3de8d963de924bce05d
Elison-schumacher <elisonschumacher30@gmail.com>
2023-09-13 15:07:18 -0300

    informando value no euro e dolar

--- scripts.js
+++ scripts.js
@@ -1,12 +1,14 @@
 const convertButton = document.querySelector(".convertButton")
+const currencySelect = document.querySelector(".currency-select")
 
 function convertValues() {
     const input = document.querySelector(".input").value //valor que colocar no conversor 
     const coinVelue = document.querySelector(".coin-converter") //valor em REAL
     const coinConverter = document.querySelector(".coin-moedas") //valor em DÓLAR 
 
-
+    console.log(currencySelect.value)
     const dolarToday = 5.20
+    const euroToday = 6.20
 
     const covertedValue = input / dolarToday
 
